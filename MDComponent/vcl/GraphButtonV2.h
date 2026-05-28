41f9e8604b7f0844ad632b16fb9416b792d85fdd
Shawn <shawn.huang@mdbs.com.tw>
2026-05-28 16:44:43 +0800

    完成 GraphButtonV2

--- MDComponent/vcl/GraphButtonV2.h
+++ MDComponent/vcl/GraphButtonV2.h
@@ -11,52 +11,54 @@
 class PACKAGE TGraphButtonV2 : public TCustomControl
 {
 private:
-		int             FID;
-		bool            FIsFocused;
-		bool            CaptureFlag;
-		bool            SelectedFlag;
-		String          CaptionText;
-		CaptionAlign    AlignType;
-		ButtonStyle     FBtnStyle;
-		TFontAttrib    *FFontInfo;
-		TNotifyEvent    FOnMouseMoveIn;
-		TNotifyEvent    FOnMouseMoveOut;
-		TWinControl    *ParentWin;
-		TPicture	   *FNormalGraph;
-		TPicture	   *FHoverGraph;
-		TPicture	   *FDownGraph;
-		TPicture	   *FDisableGraph;
-		TPicture	   *FSelectedGraph;
+		int          FID;
+		bool         FIsFocused;
+		bool         CaptureFlag;   // mouse is over the button
+		bool         FMouseDown;    // left button is pressed
+		bool         SelectedFlag;
+		String       CaptionText;
+		CaptionAlign AlignType;
+		TFontAttrib *FFontInfo;
+		TNotifyEvent FOnMouseMoveIn;
+		TNotifyEvent FOnMouseMoveOut;
+		TPicture    *FNormalGraph;
+		TPicture    *FHoverGraph;
+		TPicture    *FDownGraph;
+		TPicture    *FDisableGraph;
+		TPicture    *FSelectedGraph;
+
 		void __fastcall InitialGraph(void);
-		void __fastcall SetNormalGraph(TPicture* graph);
-		void __fastcall SetHoverGraph(TPicture* graph);
-		void __fastcall SetDownGraph(TPicture* graph);
+		void __fastcall SetGraph(TPicture* &target, TPicture* graph);
+		void __fastcall SetNormalGraph (TPicture* graph);
+		void __fastcall SetHoverGraph  (TPicture* graph);
+		void __fastcall SetDownGraph   (TPicture* graph);
 		void __fastcall SetDisableGraph(TPicture* graph);
 		void __fastcall SetSelectedGraph(TPicture* graph);
-		void __fastcall SetBtnStyle(ButtonStyle Value);
 		void __fastcall SetSelectFlag(bool Value);
 		void __fastcall SetText(const String Value);
 		void __fastcall SetAlignType(CaptionAlign Value);
 		void __fastcall SetFontAttrib(TFontAttrib *Value);
-		void __fastcall WMLosingCapture(TMessage &Msg);
-		void __fastcall WMEraseBkgnd(TMessage &Msg);
-		void __fastcall PaintButton(TColor Front, TColor Back, ButtonState State);
-		void __fastcall FontPropertyChange(TObject* Sender);
-		void __fastcall GraphChanged(TObject* Sender);
 		void __fastcall SetFocused(bool Value);
+		void __fastcall GraphChanged(TObject* Sender);
+		void __fastcall FontPropertyChange(TObject* Sender);
 		void __fastcall PaintParent(void);
+		void __fastcall DrawCaption(void);
 		void __fastcall Paint(void);
+		void __fastcall WMEraseBkgnd(TMessage &Msg);
+		void __fastcall WMLosingCapture(TMessage &Msg);
 		void __fastcall WndProc(TMessage &Msg);
 		DYNAMIC void __fastcall MouseMove(TShiftState Shift, int X, int Y);
 		DYNAMIC void __fastcall MouseDown(TMouseButton Button, TShiftState Shift, int X, int Y);
-		DYNAMIC void __fastcall MouseUp(TMouseButton Button, TShiftState Shift, int X, int Y);
-		DYNAMIC void __fastcall KeyDown(Word &Key, TShiftState Shift);
-		DYNAMIC void __fastcall Click(void);
+		DYNAMIC void __fastcall MouseUp  (TMouseButton Button, TShiftState Shift, int X, int Y);
+		DYNAMIC void __fastcall KeyDown  (Word &Key, TShiftState Shift);
+		DYNAMIC void __fastcall Click    (void);
 		__property bool IsFocused = { read = FIsFocused, write = SetFocused };
+
 protected:
 public:
 		__fastcall TGraphButtonV2(TComponent* Owner);
 		__fastcall virtual ~TGraphButtonV2(void);
+
 __published:
 		__property TabStop;
 		__property TabOrder;
@@ -73,20 +75,19 @@ __published:
 		__property OnClick;
 		__property Enabled;
 		__property Anchors;
-		__property ButtonStyle BtnStyle = {read = FBtnStyle, write = SetBtnStyle, default = bsHotTrack};
 		__property int ButtonID = {read = FID, write = FID, index = -1};
-		__property TPicture* NormalGraph 	= { read = FNormalGraph		, write = SetNormalGraph};
-		__property TPicture* HoverGraph 	= { read = FHoverGraph		, write = SetHoverGraph };
-		__property TPicture* DownGraph 		= { read = FDownGraph		, write = SetDownGraph };
-		__property TPicture* DisableGraph 	= { read = FDisableGraph	, write = SetDisableGraph };
-		__property TPicture* SelectedGraph	= { read = FSelectedGraph	, write = SetSelectedGraph};
-		__property TFontAttrib* FontAttrib = {read = FFontInfo, write = SetFontAttrib};
-		__property bool Selected = {read = SelectedFlag, write = SetSelectFlag, default = false};
-		__property CaptionAlign TextAlign = {read = AlignType, write = SetAlignType, default = caCenter};
-		__property String ButtonText = {read = CaptionText, write = SetText, nodefault};
-		__property TNotifyEvent OnMouseMoveIn = {read = FOnMouseMoveIn, write = FOnMouseMoveIn, default = NULL};
-		__property TNotifyEvent OnMouseMoveOut = {read = FOnMouseMoveOut, write = FOnMouseMoveOut, default = NULL};
-		__property bool IsMouseOver = {read = CaptureFlag};
+		__property TPicture* NormalGraph    = { read = FNormalGraph,   write = SetNormalGraph  };
+		__property TPicture* HoverGraph     = { read = FHoverGraph,    write = SetHoverGraph   };
+		__property TPicture* DownGraph      = { read = FDownGraph,     write = SetDownGraph    };
+		__property TPicture* DisableGraph   = { read = FDisableGraph,  write = SetDisableGraph };
+		__property TPicture* SelectedGraph  = { read = FSelectedGraph, write = SetSelectedGraph};
+		__property TFontAttrib* FontAttrib  = { read = FFontInfo,      write = SetFontAttrib   };
+		__property bool Selected            = { read = SelectedFlag,   write = SetSelectFlag, default = false };
+		__property CaptionAlign TextAlign   = { read = AlignType,      write = SetAlignType,  default = caCenter };
+		__property String ButtonText        = { read = CaptionText,    write = SetText, nodefault };
+		__property TNotifyEvent OnMouseMoveIn  = { read = FOnMouseMoveIn,  write = FOnMouseMoveIn,  default = NULL };
+		__property TNotifyEvent OnMouseMoveOut = { read = FOnMouseMoveOut, write = FOnMouseMoveOut, default = NULL };
+		__property bool IsMouseOver = { read = CaptureFlag };
 };
 //---------------------------------------------------------------------------
 #endif
